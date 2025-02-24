10b0983bfef11a2ce4bbe333055085b876e7ce73
damshl <damshly@proton.me>
2025-02-24 19:40:56 +0000

    update project structure

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import userRoutes from "./users.routes";
-import postRoutes from "./posts.routes";
 import authRoutes from "./auth.routes";
 import accountRoutes from "./account.routes"
 import { postController } from "../controllers/posts.controller";
@@ -9,6 +8,5 @@ const router = Router();
 router.use("/auth", authRoutes);
 router.use("/account",accountRoutes)
 router.use("/users", userRoutes);
-router.use("/posts", postRoutes);
 
 export default router;
