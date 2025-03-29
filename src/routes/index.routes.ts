8c8551af4a1694e365abefcbebef63fcb886f289
damshly <damshly@proton.me>
2025-03-29 11:36:16 +0400

    dont care

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -5,6 +5,8 @@ import accountRoutes from "./account.routes"
 import postRoutes from "./post.routes";
 const router = Router();
 
+console.log("hi");
+
 router.use("/auth", authRoutes);
 router.use("/account",accountRoutes)
 router.use("/users", userRoutes);
